f00dfa4c6716851a884018fe1e3cfe1e034b96a9
Rasul0819 <rasulabdikerimov87@gmail.com>
2024-08-20 17:36:15 +0500

    login-sign_up-logout

--- myblog/urls.py
+++ myblog/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 from .views import *
 urlpatterns = [
     path('',homepage,name='home'),
-    path('reg/',registration,name='reg')
+    path('reg/',registration,name='reg'),
+    path('login/',sing_in,name='login'),
+    path('logout/',log_out,name='logout'),
+    path('detail/<int:id>',detail,name='detail'),
 
 ]
 #     path('')
--- myblog/views.py
+++ myblog/views.py
@@ -21,3 +21,23 @@ def registration(request):
         form  = forms.RegForm()
     return render(request,'reg.html',{'form':form})
 
+def sing_in(request):
+    if request.method =='POST':
+        form  = forms.LoginForm(request,data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request,user)
+            return redirect('home')
+    else:
+        form  = forms.LoginForm()
+    return render(request,'login.html',{'form':form})
+
+
+def log_out(request):
+    logout(request)
+    return redirect('home')
+
+
+def detail(request,id):
+    post = get_object_or_404(models.Blogs,id=id)
+    return render(request,'detail.html',{'post':post})
